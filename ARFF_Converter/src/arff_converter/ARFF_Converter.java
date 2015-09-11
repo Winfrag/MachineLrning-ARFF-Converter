cdaaebdf901e2e9ed07f35992e29d0df0339f13d
Philip Wipf <philwipf@runbox.com>
2015-09-10 21:42:09 -0600

    Created ArffFile class

--- ARFF_Converter/src/arff_converter/ARFF_Converter.java
+++ ARFF_Converter/src/arff_converter/ARFF_Converter.java
@@ -17,36 +17,31 @@ import java.util.logging.Logger;
  */
 public class ARFF_Converter {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-try {
-   //URL url = new URL("http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data");
-   Scanner s = new Scanner(new File("C:/Users/Magpie/Downloads/WekaData/iristest.data"));
-   s.useDelimiter("[\n,]+");
-   while (s.hasNext())
-   {
-   System.out.print("<"+s.next()+"> ");
-   }
-   // read from your scanner
-}
-catch(IOException ex) {
-  System.err.println("IOException");
-   // there was some connection problem, or the file did not exist on the server,
-   // or your URL was not in the right format.
-   // think about what to do now, and put it here.
-   ex.printStackTrace(); // for now, simply output it.
-}
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
 
-System.out.println();
+    try {
+      //URL url = new URL("http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data");
+      Scanner s = new Scanner(new File("C:/Users/Magpie/Downloads/WekaData/iristest.data"));
+      s.useDelimiter("\n");
+      StringBuilder sb=new StringBuilder();
+      while (s.hasNext())
+      {
+
+      }
+      // read from your scanner
+    }
+    catch(IOException ex) {
+      System.err.println("IOException");
+       // there was some connection problem, or the file did not exist on the server,
+       // or your URL was not in the right format.
+       // think about what to do now, and put it here.
+       ex.printStackTrace(); // for now, simply output it.
     }
 
-
-
-
-
-
+    System.out.println();
+  }
 
 }
